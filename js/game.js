f0af4de5bf70b6bb3a368f8a8d77ec6841034f26
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-22 23:48:19 +0530

    added wheel , pin , stand

--- js/game.js
+++ js/game.js
@@ -2,7 +2,7 @@
 let config = {
     type : Phaser.CANVAS,
     width : 700,
-    height : 700,
+    height : 500,
 
     scene : {
         preload : preload,
@@ -15,10 +15,15 @@ let config = {
 let game = new Phaser.Game(config)
 
 function preload() {
-    //returns its current object : scence and shows all the available objects/functions
+    //this returns its current object : scence and shows all the available objects/functions
     // console.log(this);
-  //load the background
+
+  //load images
+  //parameters : (name , url)
   this.load.image('bg' ,'../assets/bg.jpg')
+  this.load.image('wheel' , '../assets/wheel.png')
+  this.load.image('stand' , '../assets/stand.png')
+  this.load.image('pin' , '../assets/pin.png')
 }
 
 function create() {
@@ -32,6 +37,29 @@ function create() {
     background.setPosition(W/2 , H/2);
     //adjust the size of the image and its scales both width and height 
     background.setScale(0.25);
+
+
+
+//create pin 
+let pin = this.add.sprite(0,0, 'pin')
+pin.setPosition(W/2 , H/6)
+pin.setScale(0.15);
+pin.depth = 1; // default is zero and higher than 0 appears first and lower at the back 
+
+
+
+//create stand
+let stand = this.add.sprite(0,0,'stand')
+stand.setPosition(W/2 ,(H/2)+170)
+stand.setScale(0.25);
+
+//create  wheel     
+    let wheel = this.add.sprite(0,0, 'wheel')
+    wheel.setPosition(W/2 , (H/2)-15);
+    wheel.setScale(0.15);
+
+
+
 }
 
 
