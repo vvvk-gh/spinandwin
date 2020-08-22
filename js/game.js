4ae7e084798c5cf74dfff0331435fd254d0482d8
vvvk-gh <vamsivutukuri1997@gmail.com>
2020-08-22 17:43:36 +0530

    added background

--- js/game.js
+++ js/game.js
@@ -1,6 +1,4 @@
-//basic single page game = phaser hello world
-//this setup is basic for every game you code with phaser
-
+//config
 let config = {
     type : Phaser.CANVAS,
     width : 700,
@@ -17,15 +15,28 @@ let config = {
 let game = new Phaser.Game(config)
 
 function preload() {
-    console.log(`Inside Preload`)
+    //returns its current object : scence and shows all the available objects/functions
+    // console.log(this);
+  //load the background
+  this.load.image('bg' ,'../assets/bg.jpg')
 }
 
 function create() {
-    console.log(`Inside create`)
+    let W = game.config.width;
+    let H = game.config.height;
+    
+    //create background
+    //sprite is same as image and by default x,y axis from the center as the starting point of the image
+    let background = this.add.sprite(0,0,'bg');
+    //adjust the position
+    background.setPosition(W/2 , H/2);
+    //adjust the size of the image and its scales both width and height 
+    background.setScale(0.25);
 }
+
 
 
 //Game Loop
 function update() {
-    console.log(`Inside Game`)
+   
 }
